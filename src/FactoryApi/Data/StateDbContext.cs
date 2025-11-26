1ed2121369dccf6395c73346a5c9d64a06e005c6
akongstad <kongstad@outlook.com>
2025-11-26 19:56:36 +0100

    chore: add connection string to appsetting.Development.json and fix error in StateDbContext.cs

--- src/FactoryApi/Data/StateDbContext.cs
+++ src/FactoryApi/Data/StateDbContext.cs
@@ -7,5 +7,5 @@ public class StateDbContext : DbContext
 {
     public StateDbContext(DbContextOptions<StateDbContext> options) : base(options) { }
 
-    public DbSet<StateEvent> Postures => Set<StateEvent>();
+    public DbSet<StateEvent> StateEvents => Set<StateEvent>();
 }
